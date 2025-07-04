49991baec15fd73564b5268fc20d07a8a416fc9a
Syoius <syoius@gmail.com>
2025-07-04 19:05:09 +0800

    release: v0.9.10 (#321)
    * perf: 艨艟识别调整，新增游船选项：不变
    
    * perf: 新增选项 选择超时后行为
    
    * fix: 海里捞捞指定密探追加周瑜
    
    * perf: 简中突发情报无脑点五个
    
    * fix: 开局选2船歌
    
    * perf: 适配幽灵船及迷雾
    
    * fix: mac回滚PPOCRv4
    
    * chore: 地宫56/57代号鸢简繁体素材 (#319)
    
    * feat: 地宫扫荡57
    
    * chore: focus文本修正
    
    * chore: 更新公告 模板 主页
    
    ---------
    
    Co-authored-by: swerainy <31369710+swerainy@users.noreply.github.com>

--- configure.py
+++ configure.py
@@ -1,5 +1,5 @@
+import sys  # 1. 导入 sys 模块
 from pathlib import Path
-
 import shutil
 
 assets_dir = Path(__file__).parent / "assets"
@@ -13,10 +13,22 @@ def configure_ocr_model():
         print('请完整克隆本仓库，不要漏掉 "--recursive"，也不要下载 zip 包！')
         exit(1)
 
+    if sys.platform == "darwin":
+        zh_model_version = "ppocr_v4"
+        print("Detected macOS, will use ppocr_v4 for Chinese OCR model.")
+    else:
+        zh_model_version = "ppocr_v5"
+        print(f"Detected OS: {sys.platform}. Will use ppocr_v5 for Chinese OCR model.")
+
+    zh_model_source_path = (
+        assets_dir / "MaaCommonAssets" / "OCR" / zh_model_version / "zh_cn"
+    )
+
     zh_cn_ocr_dir = assets_dir / "resource" / "base" / "model" / "ocr"
-    if not zh_cn_ocr_dir.exists():  # copy default OCR model only if dir does not exist
+    if not zh_cn_ocr_dir.exists():
+        print(f"Copying {zh_model_version} zh_cn model to: {zh_cn_ocr_dir}")
         shutil.copytree(
-            assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
+            zh_model_source_path,
             zh_cn_ocr_dir,
             dirs_exist_ok=True,
         )
@@ -25,8 +37,9 @@ def configure_ocr_model():
 
     zh_tw_ocr_dir = assets_dir / "resource" / "zh_tw" / "model" / "ocr"
     if not zh_tw_ocr_dir.exists():
+        print(f"Copying {zh_model_version} zh_cn model to: {zh_tw_ocr_dir}")
         shutil.copytree(
-            assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
+            zh_model_source_path,
             zh_tw_ocr_dir,
             dirs_exist_ok=True,
         )
